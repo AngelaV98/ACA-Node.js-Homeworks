a92a0e3c2cc77a116cda44555bf7e8704e1c78d8
Angela <angelavardanyan1998@gmail.com>
2019-09-02 00:42:07 +0400

    ''

--- Homework1/script.js
+++ Homework1/script.js
@@ -26,5 +26,4 @@
 // 6. Given string ‘test’. Use variables and string methods and print ‘tetsetesesesesteest’
 
 let str = 'test';
-let te = str.slice(0,2);
 str.substring(0,2).concat(str.split('').reverse().join(''),str.slice(1, 3).repeat(4),str.slice(0,2),str.substring(1,4))
