c8769feab1e0c85aa4c58e375beac2b57f9397c6
Angela <angelavardanyan1998@gmail.com>
2019-12-01 00:17:08 +0400

    Creating libraryAPI

---
+++ libraryAPI/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const winston = require("./src/config/winston");
+const db = require("./src/db/db.js");
+const bookRouter = require("./src/routers/book");
+const authorRouter = require("./src/routers/author");
+
+const app = express();
+
+app.use(morgan("combined", { stream: winston.stream }));
+
+app.use(express.json());
+app.use("/api/book", bookRouter);
+app.use("/api/author", authorRouter);
+
+app.get("/", function(req, res) {
+  res.send("/ GET");
+});
+
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Server is running on ${port} port`));
